65e100bb224d09dceb1049cbdff02bf575a2f06a
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:24:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Webiste Name: [Hackerrank](https://www.hackerrank.com/)
+
+// ### Tasks
+
+// Target the top description and change “Matching developers with great companies” to ‘JSBOOTCAMP“.
+
+
+
+// Write your code here:
+
+document.querySelector('.fl-heading-text');
+<span class="fl-heading-text">​…​</span>​
+
+document.querySelector('.fl-heading-text').innerText = 'JSBOOTCAMP';
+'JSBOOTCAMP'
+
